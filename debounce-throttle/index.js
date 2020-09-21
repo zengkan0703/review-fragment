1e06da923c5a120b3d78cd95c380dabecb09cb19
zeng.kan <zeng.kan@immomo.com>
2020-09-21 17:37:15 +0800

    feat: 节流

--- debounce-throttle/index.js
+++ debounce-throttle/index.js
@@ -1,3 +1,4 @@
+// 防抖
 function debounce(func, time, immediate) {
   let timer;
   let firstDone = false;
@@ -21,4 +22,26 @@ function debounce(func, time, immediate) {
     firstDone = false;
   }
   return debounced;
+}
+
+// 节流
+function throttle(func, time) {
+  let start = 0;
+  let timer;
+  return function(...args) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    const now = Date.now();
+    if (now - start >= time) {
+      func.apply(this, args);
+      start = now;
+    } else {
+      // 离开之后再调用一次
+      timer = setTimeout(function() {
+        func.apply(this, args);
+        timer = null;
+      }, time)
+    }
+  }
 }
